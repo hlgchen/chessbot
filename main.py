2a8afa1899d8b616202e2d2da1975969364ca938
hlgchen <hailong1998@googlemail.com>
2022-01-02 16:12:58 +0100

    Reformat code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame 
+import pygame
 import chess
 import time
 import torch
@@ -6,101 +6,105 @@ from bot.bots import HaiBotLong, HaiBotLongs
 from bot.model import VFA
 
 pygame.init()
-pygame.font.init() 
-myfont = pygame.font.SysFont('Comic Sans MS', 40)
+pygame.font.init()
+myfont = pygame.font.SysFont("Comic Sans MS", 40)
 
 screen = pygame.display.set_mode((800, 800))
 
-chessboard = pygame.image.load('icons/chessboard.png')
-selection = pygame.image.load('icons/selection-box.png')
+chessboard = pygame.image.load("icons/chessboard.png")
+selection = pygame.image.load("icons/selection-box.png")
 chess_icons = dict()
-chess_icons["K"] = pygame.image.load('icons/white_king.png')
-chess_icons["Q"] = pygame.image.load('icons/white_queen.png')
-chess_icons["B"] = pygame.image.load('icons/white_bishop.png')
-chess_icons["N"] = pygame.image.load('icons/white_knight.png')
-chess_icons["R"] = pygame.image.load('icons/white_rook.png')
-chess_icons["P"] = pygame.image.load('icons/white_pawn.png')
-chess_icons["k"] = pygame.image.load('icons/black_king.png')
-chess_icons["q"] = pygame.image.load('icons/black_queen.png')
-chess_icons["b"] = pygame.image.load('icons/black_bishop.png')
-chess_icons["n"] = pygame.image.load('icons/black_knight.png')
-chess_icons["r"] = pygame.image.load('icons/black_rook.png')
-chess_icons["p"] = pygame.image.load('icons/black_pawn.png')
+chess_icons["K"] = pygame.image.load("icons/white_king.png")
+chess_icons["Q"] = pygame.image.load("icons/white_queen.png")
+chess_icons["B"] = pygame.image.load("icons/white_bishop.png")
+chess_icons["N"] = pygame.image.load("icons/white_knight.png")
+chess_icons["R"] = pygame.image.load("icons/white_rook.png")
+chess_icons["P"] = pygame.image.load("icons/white_pawn.png")
+chess_icons["k"] = pygame.image.load("icons/black_king.png")
+chess_icons["q"] = pygame.image.load("icons/black_queen.png")
+chess_icons["b"] = pygame.image.load("icons/black_bishop.png")
+chess_icons["n"] = pygame.image.load("icons/black_knight.png")
+chess_icons["r"] = pygame.image.load("icons/black_rook.png")
+chess_icons["p"] = pygame.image.load("icons/black_pawn.png")
 
-def map_chess_square_coordinates(chess_square): 
+
+def map_chess_square_coordinates(chess_square):
     """Takes chess_square and returns pixel position"""
-    x = ((chess_square%8)) * 100
-    y = (7 - (chess_square//8)) * 100 
+    x = ((chess_square % 8)) * 100
+    y = (7 - (chess_square // 8)) * 100
     return (x, y)
 
-def map_coordinates_chess_square(x, y): 
+
+def map_coordinates_chess_square(x, y):
     """Takes pixel position and returns chess square"""
-    chess_square = (x//100) + (7 - y//100) * 8
+    chess_square = (x // 100) + (7 - y // 100) * 8
     return chess_square
 
 
-def draw_board(board, screen): 
-    for chess_square, piece in  board.piece_map().items(): 
+def draw_board(board, screen):
+    for chess_square, piece in board.piece_map().items():
         screen.blit(chess_icons[str(piece)], map_chess_square_coordinates(chess_square))
 
 
-def check_selected_square(selected_square, board): 
-    """Checks if selected square is one with your chess piece. Returns true if if selected_square 
+def check_selected_square(selected_square, board):
+    """Checks if selected square is one with your chess piece. Returns true if if selected_square
     has a white chess piece, otherwise returns False.
     """
-    if selected_square is None: 
+    if selected_square is None:
         result = None
-    else: 
+    else:
         result = board.piece_at(selected_square)
-    
-    if result is None: 
+
+    if result is None:
         return False
-    else: 
+    else:
         return str(result).isupper()
 
-def promotion(move, board): 
-    """Returns 5 (queen) if the move involves a white pawn moving to the end of the board. 
+
+def promotion(move, board):
+    """Returns 5 (queen) if the move involves a white pawn moving to the end of the board.
     Each pawn that reaches the other side automatically becomes a queen.
     """
     pawn = str(board.piece_at(move[0])).lower() == "p"
     end = move[1] in range(56, 64)
-    if pawn & end: 
+    if pawn & end:
         return 5
 
 
-def update_board(move, board, last_move): 
+def update_board(move, board, last_move):
     """Makes move and updates the screen.
-    
-    params: 
+
+    params:
         - move(list/tuple): with two elements, the first element is chesssquare of piece to be moved
-                            the second element is the place to be moved to. 
+                            the second element is the place to be moved to.
         - board(chess.Board):
         - last_move(list): mutable list object in which to save the current move (becomes the last move)
                         (i.e. this is used to colorize the last move in the game)
 
-    returns: 
+    returns:
         True if move was executed successfully, otherwise False
     """
     promote = promotion(move, board)
     move_uci = chess.Move(*move, promotion=promote).uci()
-    try: 
+    try:
         board.push_uci(move_uci)
         last_move[0] = move[0]
         last_move[1] = move[1]
         move[0] = None
         move[1] = None
         return True
-    except ValueError: 
+    except ValueError:
         move[1] = None
         return False
 
 
-def bot_play_wrapper(bot, e=0.0001): 
-    """"Wrapper function that returns the function bot_play."""
+def bot_play_wrapper(bot, e=0.0001):
+    """ "Wrapper function that returns the function bot_play."""
+
     def bot_play(board, last_move):
         """bot makes play and saves move in last_move.s"""
-        outcome = board.outcome() 
-        if outcome is None:  
+        outcome = board.outcome()
+        if outcome is None:
             action_bot = bot.play(board, e=e, training=False)
             move_bot = board.parse_uci(action_bot)
             board.push(move_bot)
@@ -108,89 +112,94 @@ def bot_play_wrapper(bot, e=0.0001):
             last_move[1] = move_bot.to_square
             outcome = board.outcome()
         return outcome
+
     return bot_play
 
 
-def check_outcome(outcome, screen): 
-    """takes chess.Outcome an checks who has won (if there is a winner). 
+def check_outcome(outcome, screen):
+    """takes chess.Outcome an checks who has won (if there is a winner).
     Displays the result if there is one."""
-    if outcome is not None: 
-        if outcome.winner is None: 
+    if outcome is not None:
+        if outcome.winner is None:
             text = "You are lucky to have played draw with HaiBotLong."
-        elif outcome.winner: 
+        elif outcome.winner:
             text = "HaiBotLong has let you won."
-        else: 
+        else:
             text = "HaiBotLong has destroyed you."
 
-        textsurface = myfont.render(text, False, (255, 128, 0), (250, 250,250))
+        textsurface = myfont.render(text, False, (255, 128, 0), (250, 250, 250))
         text_rect = textsurface.get_rect(center=(400, 400))
-        screen.blit(textsurface,text_rect)
-        textsurface2 = myfont.render("press R to restart", False, (255, 128, 0), (250, 250,250))
-        screen.blit(textsurface2,(0,0))
+        screen.blit(textsurface, text_rect)
+        textsurface2 = myfont.render(
+            "press R to restart", False, (255, 128, 0), (250, 250, 250)
+        )
+        screen.blit(textsurface2, (0, 0))
 
 
-def mark_in_check(in_check_list, board, screen): 
-    """If the current side playing is in check, the square with the king is saved in 
+def mark_in_check(in_check_list, board, screen):
+    """If the current side playing is in check, the square with the king is saved in
     in_check_list, the position is marked in the game"""
     in_check_list.clear()
-    if board.is_check(): 
-        s = pygame.Surface((100,100), pygame.SRCALPHA) 
-        s.fill((255,0,0,50))      
+    if board.is_check():
+        s = pygame.Surface((100, 100), pygame.SRCALPHA)
+        s.fill((255, 0, 0, 50))
         square_set = board.pieces(6, board.turn)
-        for square in square_set: 
-            screen.blit(s, map_chess_square_coordinates(square)) 
+        for square in square_set:
+            screen.blit(s, map_chess_square_coordinates(square))
 
 
-def mark_last_move(last_move, screen): 
+def mark_last_move(last_move, screen):
     """Marks squares in last move in green."""
-    s = pygame.Surface((100,100), pygame.SRCALPHA) 
-    s.fill((0,255,0,50))      
-    for square in last_move:   
-        if square is not None: 
-            screen.blit(s, map_chess_square_coordinates(square)) 
+    s = pygame.Surface((100, 100), pygame.SRCALPHA)
+    s.fill((0, 255, 0, 50))
+    for square in last_move:
+        if square is not None:
+            screen.blit(s, map_chess_square_coordinates(square))
 
 
-def get_single_bot(path, color): 
+def get_single_bot(path, color):
     bot_vfa = VFA()
     bot_vfa.load_state_dict(torch.load(path))
     bot = HaiBotLong(color=color, vfa=bot_vfa)
     return bot
 
 
-def get_ensemble_bot(path_ls, color): 
+def get_ensemble_bot(path_ls, color):
     bot_vfa_ls = []
-    for path in path_ls: 
+    for path in path_ls:
         bot_vfa = VFA()
         bot_vfa.load_state_dict(torch.load(path))
         bot_vfa_ls.append(bot_vfa)
-    bot = HaiBotLongs(vfa_ls=bot_vfa_ls, color=color,)
+    bot = HaiBotLongs(
+        vfa_ls=bot_vfa_ls,
+        color=color,
+    )
     return bot
 
 
-
-def main():             
+def main():
     # load model, 490, 410, 690, (810?)
     # bot_path = "out/models/v8/m_490_iter_42784.ckpt"
 
     # bot_path = "out/models/v8b/m_690_iter_62338.ckpt"
     # bot = get_single_bot(bot_path, color="B")
 
-    bot_path_ls = [ 
-    "out/models/v8/m_410_iter_34868.ckpt",
-    "out/models/v8/m_490_iter_42784.ckpt",
-    # "out/models/v8b/m_680_iter_61158.ckpt",
-    "out/models/v8b/m_690_iter_62338.ckpt",
-    # "out/models/v8b/m_690_iter_62338.ckpt",
-    "out/models/v8b/m_750_iter_68319.ckpt", 
-    "out/models/v8b/m_810_iter_74550.ckpt",
-    # "out/models/v8b/m_830_iter_76270.ckpt",
-    # "out/models/v8b/m_880_iter_81177.ckpt", 
-    "out/models/v8b/m_950_iter_88179.ckpt", 
-    # "out/models/v8b/m_1010_iter_93755.ckpt", 
+    bot_path_ls = [
+        "out/models/v8/m_410_iter_34868.ckpt",
+        "out/models/v8/m_490_iter_42784.ckpt",
+        # "out/models/v8b/m_680_iter_61158.ckpt",
+        "out/models/v8b/m_690_iter_62338.ckpt",
+        # "out/models/v8b/m_690_iter_62338.ckpt",
+        # "out/models/v8b/m_750_iter_68319.ckpt",
+        "out/models/v8b/m_810_iter_74550.ckpt",
+        # "out/models/v8b/m_830_iter_76270.ckpt",
+        # "out/models/v8b/m_880_iter_81177.ckpt",
+        "out/models/v8b/m_950_iter_88179.ckpt",
+        "out/models/v8c/m_740_iter_67123.ckpt",
     ]
     bot = get_ensemble_bot(bot_path_ls, color="B")
 
-    bot_play = bot_play_wrapper(bot, e=0.0001)
+    bot_play = bot_play_wrapper(bot, e=0)
 
     move = [None, None]
     opponent_turn = False
@@ -199,11 +208,10 @@ def main():
     last_move = [None, None]
     board = chess.Board()
 
-
     running = True
-    while running: 
+    while running:
 
-        if opponent_turn: 
+        if opponent_turn:
             time.sleep(0.5)
             outcome = bot_play(board, last_move)
         opponent_turn = False
@@ -212,32 +220,31 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r: 
+                if event.key == pygame.K_r:
                     running = main()
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 selected_square = map_coordinates_chess_square(*pos)
 
-                if selected_square == move[0]: 
+                if selected_square == move[0]:
                     selected_square = None
                     move[0] = None
-                elif check_selected_square(selected_square, board): 
+                elif check_selected_square(selected_square, board):
                     move[0] = selected_square
-                elif move[0] is not None: 
+                elif move[0] is not None:
                     move[1] = selected_square
-                    if update_board(move, board, last_move): 
+                    if update_board(move, board, last_move):
                         opponent_turn = True
-
 
         screen.fill((0, 0, 0))
         screen.blit(chessboard, (0, 0))
         draw_board(board, screen)
 
-        if check_selected_square(move[0], board): 
+        if check_selected_square(move[0], board):
             screen.blit(selection, map_chess_square_coordinates(move[0]))
 
         check_outcome(outcome, screen)
-        mark_in_check(in_check_list, board, screen) 
+        mark_in_check(in_check_list, board, screen)
         mark_last_move(last_move, screen)
 
         pygame.display.update()
@@ -245,6 +252,5 @@ def main():
     return False
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
-    
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
-import torch 
+import torch
 from torch import nn
-from  torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import StepLR
 
 import chess
 
@@ -16,18 +16,18 @@ import time
 reward_spec = {
     "nothing": -1,
     "capture_dict": {
-        "p": 5, 
-        "r": 10, 
-        "n": 10, 
+        "p": 5,
+        "r": 10,
+        "n": 10,
         "b": 10,
         "q": 20,
     },
     # "enemy_capture": 0,
     "checks": 2,
-    "checked": -2, 
-    "fivefold":0,
-    "draw": 0, 
-    "win": 200, 
+    "checked": -2,
+    "fivefold": 0,
+    "draw": 0,
+    "win": 200,
     "defeat": -200,
 }
 
@@ -40,77 +40,80 @@ def load_data():
         x, y = [], []
     return x, y
 
+
 def save_data(data, file_name):
     with open(file_name, "wb") as f:
         pickle.dump(data, f)
 
 
-def get_reward(last_board, board, last_board_opp, move1, move2, white): 
+def get_reward(last_board, board, last_board_opp, move1, move2, white):
     outcome = board.outcome()
     reward = reward_spec["nothing"]
 
-    if last_board.is_capture(move1): 
+    if last_board.is_capture(move1):
         reward = reward_spec["capture_dict"].get(
             str(last_board.piece_at(move1.to_square)).lower(), 0
-        ) 
+        )
 
     if last_board_opp.is_capture(move2):
-        reward += reward_spec["capture_dict"].get(
-            str(last_board_opp.piece_at(move2.to_square)).lower(), 0
-        ) * -1
+        reward += (
+            reward_spec["capture_dict"].get(
+                str(last_board_opp.piece_at(move2.to_square)).lower(), 0
+            )
+            * -1
+        )
 
-    if last_board.gives_check(move1): 
+    if last_board.gives_check(move1):
         reward += reward_spec["checks"]
 
-    if board.is_check(): 
+    if board.is_check():
         reward += reward_spec["checked"]
-    
-    if outcome is not None: 
-        if board.is_fivefold_repetition(): 
+
+    if outcome is not None:
+        if board.is_fivefold_repetition():
             reward += reward_spec["fivefold"]
-        elif outcome.winner is None: 
+        elif outcome.winner is None:
             reward += reward_spec["draw"]
-        elif outcome.winner==white: 
+        elif outcome.winner == white:
             reward = reward_spec["win"]
-        else: 
+        else:
             reward = reward_spec["defeat"]
 
     return reward
 
 
-def continue_training_from_file(file): 
-        cont_file = file.split("/")[-1]
-        game_number = int(cont_file.split("_")[1].split("m")[-1]) + 1
-        iter_count = int(cont_file[:-5].split("_")[-1]) + 1
-        print(f"cont. game: {game_number}, iter: {iter_count}")
-        return game_number, iter_count
-    
+def continue_training_from_file(file):
+    cont_file = file.split("/")[-1]
+    game_number = int(cont_file.split("_")[1].split("m")[-1]) + 1
+    iter_count = int(cont_file[:-5].split("_")[-1]) + 1
+    print(f"cont. game: {game_number}, iter: {iter_count}")
+    return game_number, iter_count
 
-def train(run, action_vfa_file=None, evaluation_vfa_file=None, opponent_vfa_file=None): 
-    #hyperparams: 
+
+def train(run, action_vfa_file=None, evaluation_vfa_file=None, opponent_vfa_file=None):
+    # hyperparams:
     C = 20000
     gamma = 0.5
 
     iter_count = 0
     c_counter = 0
     game_number = 0
-    learning_rate = 0.002
+    learning_rate = 0.001
 
     action_vfa = VFA()
     evaluation_vfa = VFA()
     opponent_vfa = VFA()
 
-    if action_vfa_file is not None: 
+    if action_vfa_file is not None:
         print(f"action selection using: {action_vfa_file}")
         action_vfa.load_state_dict(torch.load(action_vfa_file), strict=False)
         game_number, iter_count = continue_training_from_file(action_vfa_file)
-    if evaluation_vfa_file is not None: 
+    if evaluation_vfa_file is not None:
         print(f"action evaluation using: {evaluation_vfa_file}")
         evaluation_vfa.load_state_dict(torch.load(evaluation_vfa_file), strict=False)
-    if opponent_vfa_file is not None: 
+    if opponent_vfa_file is not None:
         print(f"opponent using: {opponent_vfa_file}")
         opponent_vfa.load_state_dict(torch.load(opponent_vfa_file), strict=False)
-
 
     adam = torch.optim.Adam(action_vfa.parameters(), lr=learning_rate)
     scheduler = StepLR(adam, 100, 0.5)
@@ -127,7 +130,7 @@ def train(run, action_vfa_file=None, evaluation_vfa_file=None, opponent_vfa_file
 
     start = time.time()
 
-    for g in range(game_number, game_number + 10000): 
+    for g in range(game_number, game_number + 10000):
 
         train_white = random.randint(0, 1) == 0
         bot1.w = train_white
@@ -136,19 +139,19 @@ def train(run, action_vfa_file=None, evaluation_vfa_file=None, opponent_vfa_file
 
         # if old opponent can be beaten regularly (5times in a row) --> new opponent
         last_five_games = last_five_games[-5:]
-        if sum(last_five_games) == 5: 
+        if sum(last_five_games) == 5:
             print("************* new weights OPPONENT *************")
             bot2.load_new_vfa(action_vfa)
             print("*********** new weights EVALUATION ***********")
             evaluation_vfa.load_state_dict(action_vfa.state_dict())
-            for param in evaluation_vfa.parameters(): 
+            for param in evaluation_vfa.parameters():
                 param.requires_grad = False
             game_number = g
             c_counter = 0
             last_five_games = []
             for ad in adam.param_groups:
-                ad['lr'] = learning_rate
-        e = 1/(g-game_number+40)**0.7
+                ad["lr"] = learning_rate
+        e = 1 / (g - game_number + 40) ** 0.7
 
         replay_buffer = replay_buffer[-4000:]
 
@@ -156,68 +159,86 @@ def train(run, action_vfa_file=None, evaluation_vfa_file=None, opponent_vfa_file
         move1 = move2 = outcome = None
         avg_loss = []
 
-        while True: 
-            
-            if train_white: 
+        while True:
+
+            if train_white:
                 last_board1 = board.copy()
-                action1, features1, checked_feature1 = bot1.play(board, vfa=action_vfa, e=e, training=True)
+                action1, features1, checked_feature1 = bot1.play(
+                    board, vfa=action_vfa, e=e, training=True
+                )
                 move1 = board.parse_uci(action1)
                 board.push(move1)
                 outcome = board.outcome()
-            
-            if move2 is not None: 
-            #     # if game ended, add information for bot2 before stopping loop
+
+            if move2 is not None:
+                #     # if game ended, add information for bot2 before stopping loop
                 if outcome is not None:
-                    reward2 = get_reward(last_board2, board, last_board1, move2, move1, white=bot2.w)
+                    reward2 = get_reward(
+                        last_board2, board, last_board1, move2, move1, white=bot2.w
+                    )
                     pov_board = board if board.turn else board.mirror()
-                    replay_buffer.append((
-                            features2, 
-                            checked_feature2, 
-                            reward2, 
-                            pov_board
-                    ))
-                    pov_board.push_uci("0000") # push null move for bot2, so that it's the turn of bot1 again
+                    replay_buffer.append(
+                        (features2, checked_feature2, reward2, pov_board)
+                    )
+                    pov_board.push_uci(
+                        "0000"
+                    )  # push null move for bot2, so that it's the turn of bot1 again
                     factor = -1 if (abs(reward2) == reward_spec["win"]) else 1
-                    replay_buffer.append((features1, checked_feature1, factor * reward2, pov_board.mirror()))
+                    replay_buffer.append(
+                        (
+                            features1,
+                            checked_feature1,
+                            factor * reward2,
+                            pov_board.mirror(),
+                        )
+                    )
                     break
 
             last_board2 = board.copy()
-            action2, features2, checked_feature2 = bot2.play(board, e=e/2, training=True)
+            action2, features2, checked_feature2 = bot2.play(
+                board, e=e / 2, training=True
+            )
             move2 = board.parse_uci(action2)
             board.push(move2)
             outcome = board.outcome()
-            
-            if train_white: 
-                if move2 is not None: 
-                    reward1 = get_reward(last_board1, board, last_board2, move1, move2, white=bot1.w)
+
+            if train_white:
+                if move2 is not None:
+                    reward1 = get_reward(
+                        last_board1, board, last_board2, move1, move2, white=bot1.w
+                    )
                     pov_board = board if board.turn else board.mirror()
                     replay_buffer.append((features1, checked_feature1, reward1, board))
 
                     # if game ended, add other perspective before stopping the loop
                     if outcome is not None:
                         factor = -1 if (abs(reward1) == reward_spec["win"]) else 1
-                        pov_board.push_uci("0000") # push null move for bot1, so that it's the turn of bot2 again
-                        replay_buffer.append((
-                            features2, 
-                            checked_feature2, 
-                            factor * reward1, 
-                            pov_board.mirror()
-                        ))
+                        pov_board.push_uci(
+                            "0000"
+                        )  # push null move for bot1, so that it's the turn of bot2 again
+                        replay_buffer.append(
+                            (
+                                features2,
+                                checked_feature2,
+                                factor * reward1,
+                                pov_board.mirror(),
+                            )
+                        )
                         break
-            train_white = True # if train_white was False, bot2 starts
-            
-            if len(replay_buffer) > 500: 
+            train_white = True  # if train_white was False, bot2 starts
+
+            if len(replay_buffer) > 500:
                 # minibatch training
                 targets = []
                 features = []
                 checked_feature = []
                 minibatch = random.choices(replay_buffer, k=58) + replay_buffer[-8:]
-                for tup in minibatch: 
-                    m, x1, x2 = bot1.get_best_action(tup[3], action_vfa,e=0)
-                    if m is not None: 
+                for tup in minibatch:
+                    m, x1, x2 = bot1.get_best_action(tup[3], action_vfa, e=0)
+                    if m is not None:
                         q_vfa_old = evaluation_vfa(x1, x2)
-                        y = torch.Tensor([tup[2]]) + gamma *  q_vfa_old
-                    else: 
+                        y = torch.Tensor([tup[2]]) + gamma * q_vfa_old
+                    else:
                         y = torch.Tensor([[tup[2]]])
 
                     targets.append(y)
@@ -238,45 +259,47 @@ def train(run, action_vfa_file=None, evaluation_vfa_file=None, opponent_vfa_file
                 iter_count += 1
                 c_counter += 1
 
-                if iter_count % 20 == 0: 
+                if iter_count % 20 == 0:
                     end = time.time()
-                    diff = round(end-start, 6)
+                    diff = round(end - start, 6)
                     print(f"iter: {iter_count}, loss: {loss}, took {diff} seconds")
                     start = time.time()
                 ## after certain number of iteration: update vfa params for evaluation
-                if c_counter % C == 0: 
+                if c_counter % C == 0:
                     print("*********** new weights EVALUATION ***********")
                     evaluation_vfa.load_state_dict(action_vfa.state_dict())
-                    for param in evaluation_vfa.parameters(): 
+                    for param in evaluation_vfa.parameters():
                         param.requires_grad = False
                     for ad in adam.param_groups:
-                        ad['lr'] = learning_rate
-                
+                        ad["lr"] = learning_rate
+
                 avg_loss.append(loss)
-            else: 
+            else:
                 print("replaybuffer:", len(replay_buffer))
 
         print(f"Game {g} outcome: {outcome}")
-        if len(replay_buffer) > 500: 
+        if len(replay_buffer) > 500:
             last_five_games.append(outcome.winner == int(bot1.w))
 
-            if g % 10 == 0: 
+            if g % 10 == 0:
                 save_data(board.move_stack, f"out/games/{run}/game_{g}.dat")
-                torch.save(action_vfa.state_dict(), f"out/models/{run}/m_{g}_iter_{iter_count}.ckpt")
-                
-            logger.add_scalar("loss", sum(avg_loss)/len(avg_loss), g)
-            logger.add_scalar("number_moves", len(board.move_stack),g)
-            logger.add_scalar("number_living", len(board.piece_map()),g)
+                torch.save(
+                    action_vfa.state_dict(),
+                    f"out/models/{run}/m_{g}_iter_{iter_count}.ckpt",
+                )
+
+            logger.add_scalar("loss", sum(avg_loss) / len(avg_loss), g)
+            logger.add_scalar("number_moves", len(board.move_stack), g)
+            logger.add_scalar("number_living", len(board.piece_map()), g)
             logger.flush()
 
             scheduler.step()
 
 
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     train(
-        run="v8c",
-        action_vfa_file="out/models/v8b/m_690_iter_62338.ckpt", 
-        evaluation_vfa_file="out/models/v8b/m_690_iter_62338.ckpt", 
-        opponent_vfa_file="out/models/v8b/m_690_iter_62338.ckpt", 
-        )
+        run="v8d",
+        action_vfa_file=None,
+        evaluation_vfa_file=None,
+        opponent_vfa_file=None,
+    )
